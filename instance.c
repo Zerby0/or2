c8372b58348498b7d82547b339a2af413fc860a5
adario7 <ale.dario@os2power.com>
2025-03-11 15:53:20 +0100

    check validity of new solutions in `update_sol`

--- instance.c
+++ instance.c
@@ -1,8 +1,41 @@
 #include "tsp.h"
 #include <assert.h>
+#include <math.h>
 #include <memory.h>
+#include <stdio.h>
+
+bool check_sol(const instance* inst, int* tour, double cost) {
+	int count[inst->num_nodes];
+	memset(count, 0, sizeof(int) * inst->num_nodes);
+	for (int i = 0; i < inst->num_nodes; i++) {
+		int a = tour[i];
+		if (a < 0 || a >= inst->num_nodes) {
+			fprintf(stderr, "Invalid tour: node %d is out of range: %d\n", i, a);
+			return false;
+		}
+		count[a]++;
+	}
+	for (int i = 0; i < inst->num_nodes; i++) {
+		if (count[i] != 1) {
+			fprintf(stderr, "Invalid tour: node %d appears %d times\n", i, count[i]);
+			return false;
+		}
+	}
+	double tot = 0;
+	for (int i = 0; i < inst->num_nodes; i++) {
+		int a = tour[i], b = tour[(i + 1) % inst->num_nodes];
+		tot += get_cost(inst, a, b);
+	}
+	if (fabs(tot - cost) > EPS_COST) {
+		fprintf(stderr, "Invalid tour: provided cost %f != recomputed cost %f\n", cost, tot);
+		return false;
+	}
+	return true;
+}
 
 void update_sol(instance* inst, int* tour, double cost) {
+	assert(tour != NULL);
+	assert(check_sol(inst, tour, cost));
 	if (inst->sol_cost <= cost) return;
 	memcpy(inst->sol, tour, sizeof(int) * inst->num_nodes);
 	inst->sol[inst->num_nodes] = inst->sol[0]; // loop back
