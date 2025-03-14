ba46d227b90283aae3908cc5b1b54e4540b1931c
adario7 <ale.dario@os2power.com>
2025-03-14 16:23:30 +0100

    improve two_opt code for vns

--- main.c
+++ main.c
@@ -65,10 +65,8 @@ int main(int argc, char *argv[]) {
 
     t1 = clock();
 	if (solve_instance(inst) == -1) return -1;
-	kick_rand_3_opt(inst, 3);
     t2 = clock();
     double took = (double)(t2 - t1) / CLOCKS_PER_SEC;
-	plot_cost_iteration(FILE_COST_ITER);
 	printf("%f\n", inst->sol_cost);
     debug(5, "Time: %fs\n", took);
 
--- two_opt.c
+++ two_opt.c
@@ -11,39 +11,54 @@ static void swap(int* a, int pos1, int pos2) {
     a[pos2] = temp;
 }
 
+void invert_subtour(int* tour, int i, int j) {
+	while (i < j) {
+		swap(tour, i, j);
+		i++;
+		j--;
+	}
+}
+
+bool two_opt_once(const instance* inst, int* tour, double* cost) {
+	int best_i, best_j;
+	double best_delta = 0;
+	for (int i = 0; i < inst->num_nodes - 1; i++) {
+		for (int j = i + 1; j < inst->num_nodes; j++) {
+			int a = tour[i];
+			int b = tour[i + 1];
+			int c = tour[j];
+			int d = tour[(j + 1) % inst->num_nodes];
+			double cost_next = get_cost(inst, a, c) + get_cost(inst, b, d);
+			double cost_curr = get_cost(inst, a, b) + get_cost(inst, c, d);
+			double delta = cost_next - cost_curr;
+			if (delta < best_delta) {
+				best_delta = delta;
+				best_i = i;
+				best_j = j;
+			}
+		}
+	}
+	if (best_delta < -EPS_COST) {
+		debug(80, "2-opt move: %d <-> %d swapping (%d -- %d, %d -- %d) [%f]\n", best_i, best_j, tour[best_i], tour[best_i + 1], tour[best_j], tour[(best_j + 1) % inst->num_nodes], best_delta);
+		invert_subtour(tour, best_i + 1, best_j);
+		*cost += best_delta;
+		return true;
+	} else {
+		return false;
+	}
+}
+
 void two_opt(instance* inst) {
+	// TODO time limit
     if (inst->num_nodes < 3) {
         printf("Error: instance has less than 3 nodes\n");
         return;
     }
-    int* tour = (int*) malloc((inst->num_nodes + 1) * sizeof(int));
-    for (int i = 0; i < inst->num_nodes; i++) {
-        tour[i] = inst->sol[i];
-    }
-    tour[inst->num_nodes] = inst->sol[0];
-    double cost = inst->sol_cost;
-    bool improved = true;
-    while (improved) {
-        improved = false;
-        for (int i = 0; i < inst->num_nodes - 1; i++) {
-            for (int j = i + 1; j < inst->num_nodes; j++) {
-                int a = tour[i];
-                int b = tour[i + 1];
-                int c = tour[j];
-                int d = tour[(j + 1) % inst->num_nodes];
-				double cost_add = get_cost(inst, a, c) + get_cost(inst, b, d);
-				double cost_rem = get_cost(inst, a, b) + get_cost(inst, c, d);
-				double delta = cost_add - cost_rem;
-				if (delta < -EPS_COST) {
-                    improved = true;
-                    for (int k = 0; k < (j - i) / 2; k++) {
-                        swap(tour, i + 1 + k, j - k);
-                    }
-                    cost += delta;
-                }
-            }
-        }
-    }
+    int tour[inst->num_nodes];
+	memcpy(tour, inst->sol, inst->num_nodes * sizeof(int));
+	double cost = inst->sol_cost;
+	while (two_opt_once(inst, tour, &cost)) {
+		debug(80, "2-opt improved cost = %.2f\n", cost);
+	}
     update_sol(inst, tour, cost);
-    free(tour);
 }
